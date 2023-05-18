298b56b000542b62139de17e2cca52749be8fb5c
rtyfghvbnlndl <rtyfghvbnlndl@gmail.com>
2023-05-18 20:41:35 +0800

    0518

--- sequenceList.cpp
+++ sequenceList.cpp
@@ -14,91 +14,107 @@ void seq_list_init(seq_list &l){
     l.block = (int*)malloc(sizeof(int) * INIT_SIZE);
     for(int i = 0; i < INIT_SIZE; i++){
         l.block[i] = 0;
-    };
+    }
     l.len = 0;
     l.max = 6;
-};
+}
 
 void seq_list_test(seq_list &l){
     for(int i =0; i < l.max; i++){
         cout << l.block[i] <<endl;
-    };
+    }
      cout <<"len:"<< l.len <<"\nmax:"<<l.max<<endl;
-};
+}
 
-bool seq_list_extension(seq_list &l){
+void seq_list_extension(seq_list &l){
     int *last_block = l.block;
-    l.block = (int*)malloc(sizeof(int) * (l.max + INIT_SIZE));
-    l.max = l.max + INIT_SIZE;
+    l.block = (int*)malloc(sizeof(int) * (l.max+INIT_SIZE));
     for(int i = 0; i < l.len; i++){
         l.block[i] = last_block[i];
-    };
-    for(int i = l.len; i < l.max; i++){
+    }
+    for(int i = l.max-1; i < l.max + INIT_SIZE; i++){
         l.block[i] = 0;
-    };
-    return true;
-};
+    }
+    l.max = l.max + INIT_SIZE;
+    free(last_block);
+}
 
-bool seq_list_short(seq_list &l){
+void seq_list_short(seq_list &l){
     int* last_block = l.block;
+    
     l.block = (int*)malloc(sizeof(int) * (l.max - INIT_SIZE));
+    for(int i = 0; i < l.max - INIT_SIZE; i++){
+        l.block[i] = last_block[i];
+    }
     l.max -= INIT_SIZE;
-    return true;
-};
+    free(last_block);
+}
 
-bool seq_list_insert(seq_list &l, int index, int value){
+void seq_list_insert(seq_list &l, int index, int value){
    
     if(index <= l.len && index >= 0){
-         if(index >= l.max){
+        if(l.len+1 >= l.max){
         seq_list_extension(l);
         }
-        for(int i = l.len-1; i >= index; i--){
+        for(int i = l.len; i >= index; i--){
             l.block[i+1] = l.block[i];
-        };
+        }
         l.block[index] = value;
         l.len = l.len+1;
-        return true;
     }
     else{
-        throw "INDEX EXCEPTION";
-    };
-};
+        throw invalid_argument("Index Erro");
+    }
+}
 
-bool seq_list_pop(seq_list l, int index){
-    for(int i = index; i < l.len-1; i++){
+void seq_list_pop(seq_list &l, int index){
+    for(int i = index; i < l.len; i++){
         l.block[i] = l.block[i+1];
-        l.len -= 1;
-    };
-    if(l.len <= l.max - INIT_SIZE){
+    }
+    l.len = l.len-1;
+    if(l.len < (l.max - INIT_SIZE)){ 
         //缩短
         seq_list_short(l);
-
-    };
-    return true;
-};
+    }
+}
 
 int seq_list_index(seq_list l, int value){
     for(int i = 0; i < l.len; i++){
         if(l.block[i] == value){
             return i;
-        };
-    };
-    return -1;
-};
+        }
+    }
+    throw invalid_argument("Value Erro");
+}
 
-int seq_get_value(seq_list &l, int index){
+int seq_get_value(seq_list l, int index){
     if(index >= 0 && index < l.len){
         return l.block[index];
     }
     else{
-        throw "INDEX EXCEPTION";
-    };
-};
-
+       throw invalid_argument("Index Erro");
+    }
+}
 int main(){
+    //init
     seq_list list;
     seq_list_init(list);
     seq_list_test(list);
-    seq_list_insert(list, 0, 999);
+    //insert
+    for(int i = 0; i < 7; i++){
+        seq_list_insert(list, 0, i);
+    };
     seq_list_test(list);
-};
+    //get value by index
+    cout << "value" << seq_get_value(list, 0) << endl;
+    seq_list_test(list);
+    //get index by value
+    cout << "index" << seq_list_index(list, 3) << endl;
+    seq_list_test(list);
+    //delate a value
+    seq_list_pop(list, 0);
+    seq_list_pop(list, 0);
+    seq_list_test(list);
+
+    return 0;
+}
--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -2,8 +2,115 @@
 #include <stdlib.h>
 using namespace std;
 
-struct node{
+typedef struct node{
     node* next_node;
     int value;
-};
-typedef node
+} node, *linked_list;
+
+void linked_list_init(linked_list &l){
+    l = NULL;
+}
+
+void linked_list_insert(linked_list &l, int index, int value){
+    int current_index = 0;
+    linked_list* current_node;
+    current_node = &l;
+    //找到index对应的节点,允许进到第一个不存在的节点
+    while(current_index < index && *current_node != NULL){
+        current_node = &((*current_node) -> next_node);
+        current_index += 1;
+    } 
+    //断言找到index
+    if(index != current_index){
+        throw invalid_argument("Index Erro");
+    }
+    //判断有无后驱
+    node* next_node;
+    if(*current_node == NULL){
+        next_node = NULL;
+    }
+    else{
+        next_node = *current_node;
+    }
+    //构建新节点
+    node* new_node = (node*)malloc(sizeof(node));
+    new_node->value=value;
+    new_node->next_node=next_node;
+    *current_node = new_node;
+}
+
+int linked_list_read_value(linked_list l, int index){
+    int current_index = 0;
+    node* current_node = l;
+
+    while(current_index < index && current_node -> next_node != NULL){
+        current_node = current_node -> next_node;
+        current_index += 1;
+    }
+    //过滤超出表长和负值
+    if(current_index == index){
+        return current_node -> value;
+    }
+    else{
+        throw invalid_argument("Index Erro");
+    }
+}
+
+void linked_list_pop(linked_list &l, int index){
+    int current_index = 0;
+    linked_list* current_node = &l;
+    //不允许进入不存在的节点
+    while(current_index < index && (*current_node) -> next_node != NULL){
+        current_node = &((*current_node) -> next_node);
+        current_index += 1;
+    }
+    if (index == current_index){
+        node* node_to_free;
+        if((*current_node) -> next_node != NULL){
+            node_to_free = *current_node;
+            *current_node = (*current_node) -> next_node;
+        }
+        else{
+            *current_node = NULL;
+        }
+        free(node_to_free);
+    }
+    else{
+        throw invalid_argument("Index Erro.");
+    }
+}
+
+int linked_list_index(linked_list l, int value){
+    int current_index = 0;
+    node* current_node = l;
+    if(current_node != NULL && value == current_node -> value){
+        return current_index;
+    }
+    while(current_node -> next_node != NULL){
+        current_index += 1;
+        current_node = current_node -> next_node;
+        if(value == current_node -> value){
+            return current_index;
+        }
+    }
+    throw invalid_argument("Value Erro.");
+}
+
+int main(){
+    linked_list a;
+    linked_list_insert(a, 0, 100);
+    linked_list_insert(a, 1, 200);
+    cout << linked_list_read_value(a, 0) <<endl;
+    linked_list_pop(a, 0);
+    cout << linked_list_read_value(a, 0) <<endl;
+    cout << linked_list_index(a, 200) <<endl;
+
+    
+    for(int i = 0; i < 10; i++){
+        linked_list_insert(a, 0, i);
+    }
+    for(int i = 0; i < 11; i++){
+       
+        cout << linked_list_read_value(a, i) <<endl;
+    }
+}
